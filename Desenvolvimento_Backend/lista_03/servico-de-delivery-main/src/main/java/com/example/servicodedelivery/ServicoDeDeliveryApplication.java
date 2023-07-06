d1ad33f1aa35bfecde6322b4060054b53f84be46
RafaelC97 <rafael.castro97@hotmail.com>
2023-07-05 21:48:43 -0300

    Exercicios da lista 03

---
+++ Desenvolvimento_Backend/lista_03/servico-de-delivery-main/src/main/java/com/example/servicodedelivery/ServicoDeDeliveryApplication.java
@@ -0,0 +1,14 @@
+package com.example.servicodedelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ServicoDeDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicoDeDeliveryApplication.class, args);
+    }
+
+}
---
+++ Desenvolvimento_Frontend/lista_03/ex01_BACKEND.md
@@ -0,0 +1,13 @@
+No arquivo de BACKEND.md explique com suas palavras os seguintes tópicos:
+
+    - Quais as responsabilidades de uma aplicação backend?
+
+
+    - O que é uma string de conexão com o banco de dados? (veja o código das API'S desenvolvidas em aula caso não lembre)
+
+
+    - BACKEND está do lado do CLIENTE ou SERVIDOR?
+
+
+    - Qual framework estamos utilizando para desenvolver no lado do servidor?
+
