2c01ceaf67acd71401b916d8164276090fbacdac
RafaelC97 <rafael.castro97@hotmail.com>
2023-07-14 15:01:52 -0300

    Exercicios da lista 04

--- Desenvolvimento_Frontend/lista_04/ex01/src/app/app-routing.module.ts
+++ Desenvolvimento_Frontend/lista_04/ex01/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/views/home/home.component';
+import { LoginComponent } from './components/views/login/login.component';
+import { CadastroComponent } from './components/views/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'cadastro', component: CadastroComponent},
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Desenvolvimento_Frontend/lista_04/ex01/src/app/app.module.ts
+++ Desenvolvimento_Frontend/lista_04/ex01/src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { LoginComponent } from './components/views/login/login.component';
+import { CadastroComponent } from './components/views/cadastro/cadastro.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ListagemAnimaisComponent } from './components/shared/listagem-animais/listagem-animais.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CadastroComponent,
+    NavbarComponent,
+    ListagemAnimaisComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
