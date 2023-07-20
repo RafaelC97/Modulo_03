714a080072116b514ecd15d7521ff6806e8e473c
RafaelC97 <rafael.castro97@hotmail.com>
2023-07-20 17:48:42 -0300

    Exercicios da lista 05

---
+++ Desenvolvimento_Frontend/lista_05/ex01_api_pokemons/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListagemPokemonComponent } from './components/listagem-pokemon/listagem-pokemon.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'pokemon', component: ListagemPokemonComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
